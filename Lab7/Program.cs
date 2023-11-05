14e29ccc2a0c3970195fc2bd4e84a41c0b3cec16
skylarpearcy <108814942+skylarpearcy@users.noreply.github.com>
2023-11-05 09:40:35 -0500

    Add project files.

---
+++ Lab7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userInput;
+            Console.WriteLine("Enter a number of stars:  ");
+            userInput = Console.ReadLine();
+            int.TryParse(userInput, out int numStars);
+            if (numStars <= 0)
+            {
+                Console.WriteLine("Sorry, invalid input.");
+            }
+            else
+            {
+                ShowSquareOfStars(numStars);
+            }
+
+
+        }
+        static void ShowSquareOfStars(int numStars)
+        {
+            string star = "*";
+
+            for (int x = 0; x < numStars; ++x)
+            {
+                for (int y = 0; y < numStars;  ++y)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+    }
+    
+}
